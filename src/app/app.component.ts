ac51f30c3803b7cc64a94195b5ece3c1adca0795
Halah <iHalah5@gmail.com>
2024-06-11 18:06:22 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Products } from './products';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-angularapp';
+  title = 'Qais Library';
+  inputsearch = "";
+  carticon = "assets/images/basket-cart-icon.png";
+  qty = 0;
+  cart = [];
+
+
+  mobiles: Products[]= [
+    {id: 1, name: "ايفون", price: 5600, imge: "assets/images/iphone.png"},
+    {id: 2, name: "سامسونج", price: 4800, imge: "assets/images/samsung.png"},
+    {id: 3, name: "موتورولا edge 50 Ultra 5G", price: 2999, imge: "assets/images/motorolla.jpg"},
+  ]
+
+  search(mobiles: any[]){
+    return mobiles.filter(item=> item.name.includes(this.inputsearch))
+  }
+
+  addToCard(){
+    this.qty++;
+  }
 }
+//Halah345
+
---
+++ src/app/products.ts
@@ -0,0 +1,9 @@
+//sudo ng g i Products
+//sudo ng gernerate interface Products
+//new Project -> ng new foldernamee --no-standalone
+export interface Products {
+    id: number,
+    name: string,
+    price: number
+    imge: string
+}
